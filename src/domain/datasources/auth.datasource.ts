c1f53c11cada435521e5f198cc94f303421cd4e2
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-08-11 11:16:15 -0600

    feat: update loginUser method to return UserEntity and modify RegisterUser use case to return UserCreated

--- src/domain/datasources/auth.datasource.ts
+++ src/domain/datasources/auth.datasource.ts
@@ -5,5 +5,5 @@ import { LoginUserDto } from "../dtos/auth/login-user.dto";
 
 export abstract class AuthDataSource {
   abstract registerUser(registerUserDTO: RegisterUserDto): Promise<UserEntity>;
-  abstract loginUser(loginUserDTO: LoginUserDto): Promise<any>; 
+  abstract loginUser(loginUserDTO: LoginUserDto): Promise<UserEntity>;
 }
--- src/domain/use-cases/auth/register-user.use-case.ts
+++ src/domain/use-cases/auth/register-user.use-case.ts
@@ -1,43 +1,31 @@
-import { SignOptions } from 'jsonwebtoken';
-import { JwtAdapter } from '../../../config';
 import { AuthRepository, CustomError, RegisterUserDto } from '../../';
 
-type SignToken = (payload: Object, duration: SignOptions['expiresIn']) => Promise<string | null>;
-
-interface UserToken {
-  token: string;
-  user: { 
-    id: string;
-    name: string;
-    email: string;
-  }
+interface UserCreated {
+  id: string;
+  name: string;
+  email: string;
 }
 
 interface RegisterUserUseCase {
-  execute(registerUserDTO: RegisterUserDto): Promise<UserToken>;
+  execute(registerUserDTO: RegisterUserDto): Promise<UserCreated>;
 }
 
 export class RegisterUser implements RegisterUserUseCase {
   constructor(
-    private readonly authRepository: AuthRepository,
-    private readonly signToken: SignToken = JwtAdapter.generateToken
+    private readonly authRepository: AuthRepository
   ) { }
 
-  async execute(registerUserDTO: RegisterUserDto): Promise<UserToken> {
+  async execute(registerUserDTO: RegisterUserDto): Promise<UserCreated> {
     const user = await this.authRepository.registerUser(registerUserDTO);
-    const token = await this.signToken({ id: user.id, email: user.email }, '1h');
 
-    if (!token) {
-      throw CustomError.internalServerError('Failed to generate token');
+    if (!user) {
+      throw CustomError.notFound('User registration failed');
     }
 
     return {
-      token: token,
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      } 
+      id: user.id,
+      name: user.name,
+      email: user.email,
     }
   }
 }  
